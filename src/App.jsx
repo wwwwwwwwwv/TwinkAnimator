dd4374b4806efbc2db9c2012aeae07203b8643db
wwwwwwv <ramazanovamal2@gmail.com>
2024-06-03 15:02:30 +0500

    fghjkl

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,6 @@ const App = () => {
         <Route path="/hero" element={<Hero />} />
         
       </Routes>
-
-
-
-
-      
-
-
-
-
-      
-  
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,14 +29,18 @@ const Header = () => {
     <>
       <header className="text-gray-600 body-font ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <a className= "   flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <img width={'80px'}  src="https://svgx.ru/svg/2450097.svg" alt="" />
-            <span className="ml-3 text-xl">Anitwomation</span>
+            <span className=" ml-3 text-xl">Anitwomation</span>
           </a>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center gap-5">
+
+       
+
+          <nav  className="  md:ml-auto flex flex-wrap items-center text-base justify-center gap-5">
+
             {
               links.map(c => (
-                <Link to={c.path} key={c}>{c.name}</Link>
+                <Link className='hover:text-red-600'  to={c.path} key={c}>{c.name}</Link>
               ))
              }
           </nav>
