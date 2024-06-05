3a322037ff0f13043788c70c7d25d4bc3206c8b7
wwwwwwv <ramazanovamal2@gmail.com>
2024-06-05 12:57:07 +0500

    fghjkl

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -69,21 +69,21 @@ const Products = () => {
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
         <a class="block relative h-48 rounded overflow-hidden">
-        <iframe class="object-cover object-center w-full h-full block" width="480" height="360" src="https://www.youtube.com/embed/_I_FXw0ucPk?list=PLCeVfp2xB-CM1uhaZwlfIL_WRpkvYBbPc" title="Pivot Tutorial 001: 3D kick (Basic)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe class="object-cover object-center w-full h-full block" width="611" height="343" src="https://www.youtube.com/embed/adbpm2I1yPY" title="Slipmation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">YouTube</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Pivot Tutorial 001: 3D kick (Basic)
+          <h2 class="text-gray-900 title-font text-lg font-medium">Slipmation
 </h2>
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
         <a class="block relative h-48 rounded overflow-hidden">
-        <iframe  class="object-cover object-center w-full h-full block" width="640" height="360" src="https://www.youtube.com/embed/1PFJPg9BhSw?list=PLCeVfp2xB-CM1uhaZwlfIL_WRpkvYBbPc" title="Pivot 4 Collaboration" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe class="object-cover object-center w-full h-full block" width="611" height="343" src="https://www.youtube.com/embed/sPwCIugdI2M" title="Electrozart" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </a>
         <div class="mt-4">
           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">YouTube</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">Pivot 4 Collaboration</h2>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Animator</h2>
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
